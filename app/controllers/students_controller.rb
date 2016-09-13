817c26e59f33214cf491d90f3d8bbf392b6f9825
Liam Willey <rymei10@gmail.com>
2016-09-12 21:12:43 -0700

    change controllers and routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,13 @@
 class StudentsController < ApplicationController
 
+  def new
+
+  end
+
+  def create
+    
+  end
+
   def show
     @student = Unirest.get("http://localhost:3000/api/v1/students/#{params[:id]}")
   end
