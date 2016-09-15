96784f612b7fc561b8dee756655677d1d485d497
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-09-14 21:33:02 -0700

    make functional show page

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,13 @@
 class ResumesController < ApplicationController
 
   def show
-    @student = Unirest.get("http://localhost:3000/api/v1/resumes/#{params[:id]}")
+    @student = Unirest.get("https://sheltered-harbor-10108.herokuapp.com/resumes/#{params[:id]}",
+      headers: {
+        Accept: "application/json",
+        Authorization: "#{ENV['TOKEN']}"
+      },
+    ).body
+
   end
 
   def edit 
@@ -9,18 +15,25 @@ class ResumesController < ApplicationController
   end
 
   def update
-    @student = Unirest.post("http://localhost:3000/api/v1/resumes/#{params[:id]}", parameters: {first_name: params[:first_name],
-                   last_name: params[:last_name],
-                   email: params[:email],
-                   phone_number: params[:phone_number],
-                   short_bio: params[:short_bio],
-                   linkedin: params[:linkdedln_url],
-                   twitter_handle: params[:twitter_handle],
-                   website: params[:website],
-                   resume: params[:resume],
-                   github: params[:github],
-                   photo: params[:photo]
-                   })
+    @student = Unirest.post("https://sheltered-harbor-10108.herokuapp.com/resumes/#{params[:id]}",
+      headers: {
+        Accept: "application/json",
+        Authorization: "#{ENV['TOKEN']}"
+      },
+      parameters: {
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        email: params[:email],
+        phone_number: params[:phone_number],
+        short_bio: params[:short_bio],
+        linkedin: params[:linkdedln_url],
+        twitter_handle: params[:twitter_handle],
+        website: params[:website],
+        resume: params[:resume],
+        github: params[:github],
+        photo: params[:photo]
+      }
+    ).body
   end
 
 
