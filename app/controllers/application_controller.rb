675917ad9016ebb854851146e7e964079fa712f1
Liam Willey <wwilleymc@gmail.com>
2016-09-14 18:46:44 -0700

    add sessions controller and html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :human_date_and_time
+
+
+  def human_date_and_time(date)
+    date.strftime("%b %e, %l:%M %p")
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def authenticate_user!
+    redirect_to '/login' unless current_user
+  end
+
 end
