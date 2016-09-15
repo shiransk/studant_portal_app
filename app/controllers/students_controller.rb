117875a2b2b3b9a4a97287ebc3ffbec8c995ed87
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-09-14 19:34:25 -0700

    add api key generator to new action for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,11 +6,16 @@ class StudentsController < ApplicationController
 
   def create
 
+    api_key = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+
     student_hash = {
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-
+      phone_number: params[:phone_number],
+      api_key: api_key,
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
     }
 
     @student = Unirest.post("http://localhost:3000/resumes",
