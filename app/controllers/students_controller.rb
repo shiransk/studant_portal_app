9d854d065a96db8cf4b7df5b5efffcbfa2576999
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-09-14 19:56:01 -0700

    add token to env

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,14 +6,11 @@ class StudentsController < ApplicationController
 
   def create
 
-    api_key = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
-
     student_hash = {
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
-      api_key: api_key,
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     }
@@ -21,7 +18,7 @@ class StudentsController < ApplicationController
     @student = Unirest.post("http://localhost:3000/resumes",
       headers: {
         Accept: "application/json",
-        Authorization: "Token token = #{ENV['API-KEY']}"
+        Authorization: "#{ENV['TOKEN']}"
       },
       parameters: student_hash
     )
